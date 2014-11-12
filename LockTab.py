f242cf3585486fec22805411bdc9df8231925794
David G <davidgerva@gmail.com>
2014-11-12 16:40:21 +0100

    Initial commit

---
+++ LockTab.py
@@ -0,0 +1,134 @@
+import sublime, sublime_plugin
+
+sSettings = sublime.load_settings('LockTab.sublime-settings')
+
+# For ST3
+def plugin_loaded():
+   global window
+   window = sublime.windows()[0]
+   global sSettings
+   sSettings = sublime.load_settings('LockTab.sublime-settings')
+
+   # Loading lockList and Alert settings
+   tempLockList = sSettings.get("locked")
+   tempAlert    = sSettings.get("Alert")
+   tempFocusOCT = sSettings.get("Focus on closed tab") 
+   # Check that every "locked" file is still in the window
+   for tempFile in tempLockList:
+      found = False
+
+      for view in window.views():
+         if tempFile == view.file_name():
+            found = True
+
+      if not found:
+         tempLockList.remove(tempFile) ;
+
+   # saving settings
+   sSettings.set("locked", tempLockList)
+   sSettings.set("Alert", tempAlert)
+   sSettings.set("Focus on closed tab", tempFocusOCT)
+   sublime.save_settings( "LockTab.sublime-settings")
+
+
+class LockTabCommand( sublime_plugin.TextCommand ):
+      
+   def run(self, edit):
+
+      self.Lock_list = sSettings.get("locked")
+      settings = sublime.load_settings("LockTab.sublime-settings")
+      if not (self.view.file_name() in self.Lock_list):
+         name = self.view.name()
+         self.view.set_status("Locked", "LOCKED")
+
+         self.Lock_list.append(self.view.file_name())
+         sSettings.set("locked", self.Lock_list)
+         sublime.save_settings( "LockTab.sublime-settings")
+
+   def is_enabled(self, paths=None):
+      # Avoiding locking of not-saved file
+      self.Lock_list = sSettings.get("locked")      
+      if not ( self.view.file_name() ):
+         return False
+
+      if (self.view.file_name() in self.Lock_list):
+         return False
+
+      return True
+
+class UnLockTabCommand( sublime_plugin.TextCommand ):
+
+   def run(self, edit):
+      self.Lock_list = sSettings.get("locked")
+      if (self.view.file_name() in self.Lock_list):
+         self.Lock_list.remove(self.view.file_name())
+         self.view.erase_status("Locked")
+         sSettings.set("locked", self.Lock_list)
+         sublime.save_settings( "LockTab.sublime-settings")
+
+   # def is_visible(self, paths=None):
+   def is_enabled(self, paths=None):
+      self.Lock_list = sSettings.get("locked")
+
+      if (self.view.file_name() in self.Lock_list):
+         return True
+
+      return False
+
+class KeyBindingListener(sublime_plugin.EventListener):
+
+   def on_pre_close(self, view ):
+
+      nId = view.file_name()
+      Lock_list = sSettings.get("locked")
+      bAlert = sSettings.get("Alert")
+
+      if not (nId in Lock_list):
+         return
+
+      # Saving tab position
+      self.TabPosition = window.get_view_index(view)
+      print (str(self.TabPosition[0] ) + str(self.TabPosition[1]) )
+
+      # Saving focus position
+      self.visibleRegion = view.visible_region()
+      # saving actual active view
+      self.ActiveView = window.active_view()
+
+      self.bClose = False;
+      # PopUp
+      if (bAlert):
+         if (sublime.ok_cancel_dialog("Locked Tab. Close it anyway?", "Yep!")):
+            self.bClose = True;
+
+
+   def on_load(self, view):
+      # setting focus position
+      view.show(self.visibleRegion.begin())
+
+   def on_close(self, view):
+
+      nId          = view.file_name()
+      tempFocusOCT = sSettings.get("Focus on closed tab") 
+      Lock_list    = sSettings.get("locked")
+
+      # if tab locked
+      if (nId in Lock_list):
+
+         # if user confirm closure
+         if (self.bClose):
+            # removing filename
+            Lock_list.remove(nId)
+            # exiting
+            sublime.save_settings( "LockTab.sublime-settings")
+            return
+
+         # if I have to keep the tab opened
+         window.run_command("reopen_last_file")
+
+         # setting the old tab position
+         vNew = window.active_view()
+         window.set_view_index(vNew, self.TabPosition[0], self.TabPosition[1])
+         if not tempFocusOCT:
+            window.focus_view(self.ActiveView)
+            
--- README.md
+++ README.md
@@ -1,4 +1,43 @@
-LockTab-Sublime-Plugin
-======================
+LockTab
+========
 
-Repository for the LockTab sublime 3 plugin
+This plugin allows you to lock (and unlock) a tab in Sublime.
+The plugin tries to remember the position of the tab and also the position of the
+focus in the view.
+
+This is my first work and it has not the claim to be complete or perfect.
+(It is full of bug. ;)
+
+It is (and will remain) a very simple and essential, but I hope useful, plugin but it can
+be improved in a lot of aspects.
+
+Usage
+-----
+Just right click on the view (right now not on the tab name, see ToDo#4) and Lock/Unlock
+the tab.
+LockTab.sublime-settings is quite self-explained.
+
+
+ToDo
+----
+1. First of all I'm looking for a method for avoiding the normal behavior of sublime.
+Right now I just intercept the tab closing, and then I reopen the tab. This is, as far as I
+know, the only thing I can do. Right now is not possibile avoiding the tab closure!
+Do you know how to do it?
+
+2. One of the most important things to do is to add a particular icon (or also only a different
+colour) to the locked tab. I haven't tried it. It could be very easy or impossible. I don't know.
+
+3. Maybe I should improve the code. It just started out as a "bet" in my office. I don't know a lot
+neither of phyton nor of sublime plugins.
+
+4. It could be very useful permit the Lock/UnLock of the tab directly from the tab name
+and not only on the tab "body". Right now sublime do not give the view ID of the right-clicked
+tab name but only of the view on which you have the focus.
+Do you know how to do it?
+
+5. Maybe a better english translation of this Readme?
+
+6. Managing more than one group of views. Again didn't try at all.
+
+7. Improving the "set focus" after the reopen
